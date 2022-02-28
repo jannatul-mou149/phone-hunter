9787a24fa414067d8f4606a9e4c7a6112708b1d8
jannatul-mou149 <jannatul15-12149@gmail.com>
2022-02-28 22:12:51 +0600

    search mobile and showing on UI done

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log('hi')
+const searchMobile = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    console.log(searchText);
+
+    const url = ` https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displaySearchResult(data.data))
+}
+
+const displaySearchResult = (data) => {
+    const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
+    data.forEach(data => {
+        console.log(data);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div onclick="loadMobileDetail('${data.slug}')" class="card h-100">
+            <img src="${data.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${data.phone_name}</h5>
+            </div>
+        </div>
+        `;
+        searchResult.appendChild(div)
+    })
+}
+
+const loadMobileDetail = id => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data.data));
+}
